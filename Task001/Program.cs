537098a68af7ab706822eff763d2e44c2c8b6ff9
OvcS <gerkules007@yahoo.com>
2021-11-08 22:21:53 +0300

    Task 1 Done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,20 +1,31 @@
-﻿string[] input = System.IO.File.ReadAllLines(@"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt");
+﻿// В файле содержится последовательность из 10 000 целых положительных чисел. Каждое число не превышает 10 000.
+// Определите и запишите в ответе сначала количество пар элементов последовательности, у которых разность элементов кратна 60, 
+// затем максимальную из разностей элементов таких пар. В данной задаче под парой подразумевается два различных элемента 
+// последовательности. Порядок элементов в паре не важен.
+
+string[] input = System.IO.File.ReadAllLines(@"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt");
 // double[] arr = File.ReadAllLines(@"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt").Select(i => double.Parse(i.Trim(),NumberFormatInfo.InvariantInfo )).ToArray();
 
-int CountNumbers(double[] array1)
+int CountNumbers(int[] array1)
 {
     int i = 0;
     int count = 0;
-    for (; i < array1.Length-1; i++)
+    int a = array1[i];
+    int b = array1[i + 1];
+    for (i = 0; i < array1.Length; i++)
     {
-        if ((array1[i] - array1[i+1]) % 60 == 0)
+        a = array1[i];
+        for (int j = i + 1; j < array1.Length; j++)
         {
-            count++;
+            if ((a - array1[j]) % 60 == 0)
+            {
+                count++;
+            }
         }
     }
     return count;
 }
 
-double[] newarray = Array.ConvertAll(input, Double.Parse);
+int[] newarray = Array.ConvertAll(input, Int32.Parse);
 int result = CountNumbers(newarray);
 Console.WriteLine(result);
