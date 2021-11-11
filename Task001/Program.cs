3341839c4c91daff3a0f9a515a4bd61a11e06a5e
OvcS <gerkules007@yahoo.com>
2021-11-11 21:55:13 +0300

    Add Task2

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -7,7 +7,7 @@ int[] ImportStringArrayToInt(string place)
 {
     // Console.WriteLine("Укажите место импорта данных:");
     // string place = Console.ReadLine()!;
-    // double[] arr = File.ReadAllLines(@"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt").Select(i => double.Parse(i.Trim(),NumberFormatInfo.InvariantInfo )).ToArray();
+    // double[] arr = File.ReadAllLines(@"").Select(i => double.Parse(i)).ToArray();
     string[] input = System.IO.File.ReadAllLines(place);
     int[] newarray = Array.ConvertAll(input, Int32.Parse);
     return newarray;
@@ -17,20 +17,18 @@ int[] CountNumbers(int[] array1)
 {
     int i = 0;
     int count = 0;
-    int a = array1[i];
-    int b = array1[i + 1];
     int max = 0;
     for (i = 0; i < array1.Length - 1; i++)
     {
-        a = array1[i];
+        //a = array1[i];
         for (int j = i + 1; j < array1.Length; j++)
         {
-            if ((a - array1[j]) % 60 == 0)
+            if (((array1[i] * array1[j]) % 26 == 0))
             {
                 count++;
-                if ((a - array1[j]) > max)
+                if ((array1[i] + array1[j]) > max)
                 {
-                    max = a - array1[j];
+                    max = array1[i] + array1[j];
                 }
             }
         }
@@ -39,7 +37,7 @@ int[] CountNumbers(int[] array1)
     return (arr);
 }
 
-string directory = @"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt";
+string directory = @"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.1.txt";
 int[] newarray = ImportStringArrayToInt(directory);
 int[] result = CountNumbers(newarray);
-Console.WriteLine($"Разность элементов кратна 60 = {result[0]} и максимальную из разностей элементов таких пар = {result[1]}");
+Console.WriteLine($"Про элементов кратна 26 = {result[0]} и максимальную из сумм элементов таких пар = {result[1]}");
---
+++ Task002/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] ImportStringArrayToInt(string place)
+{
+    // Console.WriteLine("Укажите место импорта данных:");
+    // string place = Console.ReadLine()!;
+    // double[] arr = File.ReadAllLines(@"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.txt").Select(i => double.Parse(i.Trim(),NumberFormatInfo.InvariantInfo )).ToArray();
+    string[] input = System.IO.File.ReadAllLines(place);
+    int[] newarray = Array.ConvertAll(input, Int32.Parse);
+    return newarray;
+}
+
+int[] CountNumbers(int[] array1)
+{
+    int i = 0;
+    int count = 0;
+    int max = 0;
+    for (i = 0; i < array1.Length - 1; i++)
+    {
+        //a = array1[i];
+        for (int j = i + 1; j < array1.Length; j++)
+        {
+            if (((array1[i] + array1[j]) % 2 != 0) && ((array1[i] * array1[j]) % 5 == 0))
+            {
+                count++;
+                if ((array1[i] + array1[j]) > max)
+                {
+                    max = array1[i] + array1[j];
+                }
+            }
+        }
+    }
+    int[] arr = { count, max };
+    return (arr);
+}
+
+string directory = @"C:\Users\ovcse\Desktop\РАЗРАБОТЧИК\СЕМИНАРЫ\in CS\Lesson4\17.2.txt";
+int[] newarray = ImportStringArrayToInt(directory);
+int[] result = CountNumbers(newarray);
+Console.WriteLine($"Про элементов кратна 26 = {result[0]} и максимальную из сумм элементов таких пар = {result[1]}");
